5d5294608bf9627d027e4bda5e918a81f6a8cc74
masturah45 <adesanyamasturah@gmail.com>
2022-07-25 16:17:25 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace New_project_git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is my first Git project");
+            Console.WriteLine("Hello world!")
+        }
+    }
+}
